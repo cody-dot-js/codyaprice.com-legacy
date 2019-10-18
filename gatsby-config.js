5126037a17d6f4f53eafbddf0a9a5d5b97f20e78
Cody Price <Cody.Price@cerner.com>
2019-10-17 20:07:09 -0500

    fix google analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,36 +1,30 @@
 module.exports = {
   siteMetadata: {
-    title: `Cody A Price`,
-    author: `Cody Price`,
-    description: `My personal website.`,
-    siteUrl: `https://codyaprice.com/`,
+    title: "Cody A Price",
+    author: "Cody Price",
+    description: "My personal website.",
+    siteUrl: "https://codyaprice.com/",
     social: {
-      twitter: `lokcal_styles`,
+      twitter: "lokcal_styles",
     },
   },
   plugins: [
     {
-      resolve: "gatsby-plugin-google-analytics",
+      resolve: "gatsby-source-filesystem",
       options: {
-        trackingId: "UA-146469183-1",
+        path: "${__dirname}/content/blog",
+        name: "blog",
       },
     },
     {
-      resolve: `gatsby-source-filesystem`,
+      resolve: "gatsby-source-filesystem",
       options: {
-        path: `${__dirname}/content/blog`,
-        name: `blog`,
+        path: "${__dirname}/content/assets",
+        name: "assets",
       },
     },
     {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        path: `${__dirname}/content/assets`,
-        name: `assets`,
-      },
-    },
-    {
-      resolve: `gatsby-transformer-remark`,
+      resolve: "gatsby-transformer-remark",
       options: {
         plugins: [
           {
@@ -44,56 +38,56 @@ module.exports = {
             },
           },
           {
-            resolve: `gatsby-remark-images`,
+            resolve: "gatsby-remark-images",
             options: {
               maxWidth: 590,
             },
           },
           {
-            resolve: `gatsby-remark-responsive-iframe`,
+            resolve: "gatsby-remark-responsive-iframe",
             options: {
-              wrapperStyle: `margin-bottom: 1.0725rem`,
+              wrapperStyle: "margin-bottom: 1.0725rem",
             },
           },
           "gatsby-remark-autolink-headers",
           {
-            resolve: `gatsby-remark-prismjs`,
+            resolve: "gatsby-remark-prismjs",
             options: {
               showLineNumbers: true,
             },
           },
-          `gatsby-remark-copy-linked-files`,
-          `gatsby-remark-smartypants`,
+          "gatsby-remark-copy-linked-files",
+          "gatsby-remark-smartypants",
         ],
       },
     },
-    `gatsby-transformer-sharp`,
-    `gatsby-plugin-sharp`,
+    "gatsby-transformer-sharp",
+    "gatsby-plugin-sharp",
     {
-      resolve: `gatsby-plugin-google-analytics`,
+      resolve: "gatsby-plugin-google-analytics",
       options: {
-        //trackingId: `ADD YOUR TRACKING ID HERE`,
+        trackingId: "UA-146469183-1",
       },
     },
-    `gatsby-plugin-feed`,
+    "gatsby-plugin-feed",
     {
-      resolve: `gatsby-plugin-manifest`,
+      resolve: "gatsby-plugin-manifest",
       options: {
-        name: `codyaprice.com`,
-        short_name: `codyaprice`,
-        start_url: `/`,
-        background_color: `#ffffff`,
-        theme_color: `#663399`,
-        display: `minimal-ui`,
-        icon: `content/assets/icon.png`,
+        name: "codyaprice.com",
+        short_name: "codyaprice",
+        start_url: "/",
+        background_color: "#ffffff",
+        theme_color: "#663399",
+        display: "minimal-ui",
+        icon: "content/assets/icon.png",
       },
     },
-    `gatsby-plugin-offline`,
-    `gatsby-plugin-react-helmet`,
+    "gatsby-plugin-offline",
+    "gatsby-plugin-react-helmet",
     {
-      resolve: `gatsby-plugin-typography`,
+      resolve: "gatsby-plugin-typography",
       options: {
-        pathToConfigModule: `src/utils/typography`,
+        pathToConfigModule: "src/utils/typography",
       },
     },
   ],
