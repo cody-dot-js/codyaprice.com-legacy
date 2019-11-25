027af9168106cdb7e61ee0a1f9d661afd3289cc6
Cody Price <34115417+dev-cprice@users.noreply.github.com>
2019-11-25 00:23:02 -0600

    surface tags UI (#42)
    * surface tags
    
    * remove lint

---
+++ src/components/Badge.jsx
@@ -0,0 +1,34 @@
+import React from "react"
+import PropTypes from "prop-types"
+
+const propTypes = {
+  children: PropTypes.node,
+  color: PropTypes.string,
+}
+
+const defaultProps = {
+  children: null,
+  color: "#a44fb6",
+}
+
+function Badge({ children, color, ...extraProps }) {
+  return (
+    <div
+      css={{
+        padding: "0.5rem",
+        color: color,
+        borderRadius: "0.5rem",
+        border: `1px solid ${color}`,
+        fontSize: "0.75rem",
+      }}
+      {...extraProps}
+    >
+      {children}
+    </div>
+  )
+}
+
+Badge.propTypes = propTypes
+Badge.defaultProps = defaultProps
+
+export default Badge
---
+++ src/components/Tags.jsx
@@ -0,0 +1,50 @@
+import React from "react"
+import PropTypes from "prop-types"
+import Link from "./Link"
+import Badge from "./Badge"
+
+const propTypes = {
+  list: PropTypes.arrayOf(PropTypes.string),
+}
+
+const defaultProps = {
+  list: [],
+}
+
+function Tags({ list }) {
+  return (
+    <div
+      css={{
+        display: "flex",
+        flexFlow: "row wrap",
+        marginBottom: "1rem",
+      }}
+    >
+      {list.map((tag, i) => (
+        <Link
+          // TODO: make tags go to `/tags/${tag}`
+          key={tag}
+          to="#"
+          css={{
+            textDecoration: "none",
+            color: "#a44fb6",
+          }}
+        >
+          <Badge
+            css={{
+              marginTop: "0.5rem",
+              marginRight: i === list.length - 1 ? 0 : "0.5rem",
+            }}
+          >
+            {tag}
+          </Badge>
+        </Link>
+      ))}
+    </div>
+  )
+}
+
+Tags.propTypes = propTypes
+Tags.defaultProps = defaultProps
+
+export default Tags
