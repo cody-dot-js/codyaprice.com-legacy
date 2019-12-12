16804ec921853cf971dbc95f87c52ff928f96979
Cody Price <34115417+dev-cprice@users.noreply.github.com>
2019-12-12 11:42:49 -0600

    update read time estimates (#52)

--- src/utils/formatReadingTime.js
+++ src/utils/formatReadingTime.js
@@ -1,10 +1,18 @@
 export default function formatReadingTime(minutes) {
-  const cups = Math.round(minutes / 5)
+  // can't figure out how to update the `timeToRead` markdown remark
+  // query field, so I'll just pad the time here with 60% extra read
+  // time to match what I feel is the _real_ read time for most posts
+  // will iterate on it
+  const paddedMinutes = minutes / 0.4
+
+  const cups = Math.round(paddedMinutes / 5)
   if (cups > 5) {
     return `${new Array(Math.round(cups / Math.E))
       .fill("🍱")
-      .join("")} ${minutes} min read`
+      .join("")} ${paddedMinutes} min read`
   } else {
-    return `${new Array(cups || 1).fill("☕️").join("")} ${minutes} min read`
+    return `${new Array(cups || 1)
+      .fill("☕️")
+      .join("")} ${paddedMinutes} min read`
   }
 }
