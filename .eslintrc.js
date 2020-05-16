0cb64d578ad9b02587e5d96f3daa2b2cefc70010
Cody Price <dev.cprice@gmail.com>
2020-05-15 20:49:20 -0700

    Added ts dependencies and configs

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,43 @@
 module.exports = {
+  parser: "@typescript-eslint/parser",
+  extends: [
+    "react-app",
+    "plugin:@typescript-eslint/recommended",
+    "prettier/@typescript-eslint",
+    "plugin:prettier/recommended",
+  ],
+  settings: {
+    react: {
+      version: "detect",
+    },
+  },
+  env: {
+    broswer: true,
+    node: true,
+    es6: true,
+  },
+  plugins: ["typescript-eslint", "react"],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 2020,
+    sourceType: "module",
+  },
+  rules: {
+    "react/prop-types": "off", // Disable prop-types as we use TypeScript for type checking
+    "@typescript-eslint/explicit-function-return-type": "off",
+  },
+  overrides: [
+    // Override some TypeScript rules just for .js files
+    {
+      files: ["*.js"],
+      rules: {
+        "@typescript-eslint/no-var-requires": "off", //
+      },
+    },
+  ],
   globals: {
     __PATH_PREFIX__: true,
   },
-  extends: "react-app",
 }
